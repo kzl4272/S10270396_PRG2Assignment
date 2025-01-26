c7515a303b80a3a17674dfd3c91b8587a6dd2c0e
Jayden <S10267761@connect.np.edu.sg>
2025-01-26 19:35:53 +0800

    flight class added

---
+++ Flight.cs
@@ -0,0 +1,79 @@
+﻿//==========================================================
+// Student Number : S10267761
+// Student Name : Jayden Koh Kai Xuan
+// Partner Name : Kua Zi Liang
+//==========================================================
+namespace PRG_ASSG
+{
+    internal class Flight
+    {
+        private string flightNumber;
+
+        public string FlightNumber
+        {
+            get { return flightNumber; }
+            set { flightNumber = value; }
+        }
+
+
+
+        private string origin;
+
+        public string Origin
+        {
+            get { return origin; }
+            set { origin = value; }
+        }
+
+        private string destination;
+
+        public string Destination
+        {
+            get { return destination; }
+            set { destination = value; }
+        }
+
+        private DateTime expectedTime;
+
+        public DateTime ExpectedTime
+        {
+            get { return expectedTime; }
+            set { expectedTime = value; }
+        }
+
+        private string status;
+
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        public Flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+        {
+            FlightNumber = flightNumber;
+            Origin = origin;
+            Destination = destination;
+            ExpectedTime = expectedTime;
+            Status = status;
+        }
+
+        public double CalculateFees()
+        {
+            return 0;
+        }
+
+
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+
+
+
+
+    }
+}
