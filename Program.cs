cc2dc90ae292bb09cbd54b520affd7ad9374a781
kzl4272 <S10270396@connect.np.edu.sg>
2025-01-26 22:42:32 +0800

    g

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using PRG_ASSG;
-using System.Reflection.PortableExecutable;
-
+﻿using PRG_ASSG;
 
 
 /* FEATURE 1 (ZI LIANG) */
@@ -9,53 +6,7 @@ using System.Reflection.PortableExecutable;
 
 
 /* FEATURE 2 (JAYDEN) */
-Dictionary<string, Flight> flightsDictionary = new Dictionary<string, Flight>();
-void LoadFlights()
-{
-    string path = "flights.csv";
-    try
-    {
-        using (StreamReader reader = new StreamReader(path))
-        {
-            // skip header
-            reader.ReadLine();
 
-            // reads CSV file
-            while (!reader.EndOfStream)
-            {
-                string line = reader.ReadLine();
-                string[] values = line.Split(',');
-
-                //
-                if (values.Length != 5)
-                {
-                    Console.WriteLine($"Skipping invalid line: {line}");
-                    continue;
-                }
-
-                // Parse the data from the CSV file
-                string flightNumber = values[0];
-                string origin = values[1];
-                string destination = values[2];
-                DateTime expectedTime = Convert.ToDateTime(values[3]);
-                string status = "on time";
-
-                // Create a Flight object using the parsed data
-                Flight flight = new Flight(flightNumber, origin, destination, expectedTime, status);
-
-                // Add the flight to the dictionary with FlightNumber as the key
-                flightsDictionary[flight.FlightNumber] = flight;
-            }
-        }
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine("Error reading this file: " + ex.Message);
-    }
-}
-
-
-LoadFlights();
 
 
 /* FEATURE 3 (ZI LIANG) */
